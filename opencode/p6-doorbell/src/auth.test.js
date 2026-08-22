3b180eb7150856870d125737edf4f73bfa9cdcc5
FarahSyed <farahsyed44@gmail.com>
2026-08-22 12:32:07 +0500

    Revert "Add Project 6: Doorbell Loop (with planted bug)"
    This reverts commit 55b9150d9428b59ceb333fbc4705100358dc37b0.

--- opencode/p6-doorbell/src/auth.test.js
+++
@@ -1,8 +0,0 @@
-const { validatePassword } = require('./auth');
-
-test('password must be at least 8 characters', () => {
-  expect(validatePassword('short')).toBe(false);
-  expect(validatePassword('1234567')).toBe(false);
-  expect(validatePassword('12345678')).toBe(true);
-  expect(validatePassword('longpassword')).toBe(true);
-});
